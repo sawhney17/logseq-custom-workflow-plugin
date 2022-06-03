ce6c5a1a0ee57d43c7f3e61bd1fbb245ef09d38b
Aryan Sawhney <240aryans@gmail.com>
2022-06-03 10:25:59 +0400

    support text append on workflow completion

--- index.ts
+++ index.ts
@@ -1,10 +1,62 @@
 import '@logseq/libs';
 import { SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin.user';
-
+import  'logseq-dateutils';
+import { getDateForPageWithoutBrackets } from 'logseq-dateutils';
 
 let logseqSettings = logseq.settings
 const stateOptions = ["Only Change current block", "Change current block and block below"]
-async function toggleState(SeparatedArray, uuid) {
+const parseTemplate = async (template) => {
+  //Create a date object iwth today's date
+  let date = new Date()
+  let dateString = getDateForPageWithoutBrackets(date, (await logseq.App.getUserConfigs()).preferredDateFormat)
+  
+  //Replace {date} with the date
+  template = template.replaceAll(/{date}/gi, dateString)
+  //Create a time object with the current time
+  let time = new Date()
+  let timeString = formatTime()
+  //Replace {time} with the time
+  template = template.replaceAll(/{time}/gi, timeString)
+  //Replace {currentPage} with the current page
+  template = template.replaceAll(/{currentPage}/gi, await logseq.Editor.getCurrentPage())
+
+  return ` ${template}`
+
+}
+
+function formatTime() {
+  let formattedTimeStamp = new Date();
+  let initialHours = formattedTimeStamp.getHours();
+  let hours;
+  if (initialHours == 0) {
+    hours = "00";
+  } else {
+    hours = initialHours;
+    if (formattedTimeStamp.getHours() < 10) {
+      hours = "0" + formattedTimeStamp.getHours();
+    }
+  }
+  var formattedTime;
+  if (formattedTimeStamp.getMinutes() < 10) {
+    formattedTime = hours + ":" + "0" + formattedTimeStamp.getMinutes();
+  } else {
+    formattedTime = hours + ":" + formattedTimeStamp.getMinutes();
+  }
+  // if (
+  //   typeof logseq.settings.timeFormat == "undefined" ||
+  //   logseq.settings.timeFormat == "12 hour time"
+  // ) {
+  //   return new Date("1970-01-01T" + formattedTime + "Z").toLocaleTimeString(
+  //     "en-US",
+  //     { timeZone: "UTC", hour12: true, hour: "numeric", minute: "numeric" }
+  //   );
+  // } else {
+    return formattedTime;
+  // }
+
+}
+
+async function toggleState(SeparatedArray, uuid, template) {
   let block = await logseq.Editor.getBlock(uuid)
   if (block != undefined) {
     let blockFirstWord = block.content.split(' ')[0].split('\n')[0]
@@ -18,11 +70,18 @@ async function toggleState(SeparatedArray, uuid) {
           finalText = " "
           insertionTracker = true
         }
+        else if (commaSeparatedArray[index + 2] == undefined) {
+          finalText = commaSeparatedArray[index + 1]
+          insertionTracker = true
+          logseq.Editor.updateBlock(uuid, finalText + block.content.replace(blockFirstWord, '') + await parseTemplate(template))
+          break 
+        }
         else {
           finalText = commaSeparatedArray[index + 1]
           insertionTracker = true
         }
         logseq.Editor.updateBlock(uuid, finalText + block.content.replace(blockFirstWord, ''))
+        
         break
       }
     }
@@ -40,7 +99,7 @@ let settings: SettingSchemaDesc[] = [
     type: "string",
     title: "Options for Workflow 1",
     description: "Enter your desired workflow options, separated by commas. i.e. 'TODO, DOING, WAITING, CANCELED'",
-    default: "TODO, DONE, CANCELLED, WAITING"
+    default: "TODO, CANCELLED, WAITING, DONE"
   },
   {
     key: "Keyboard-Shortcut",
@@ -57,6 +116,14 @@ let settings: SettingSchemaDesc[] = [
     enumChoices: stateOptions,
     enumPicker: "radio",
     default: "Only Change current block"
+  },
+  {
+    key: "AppendOnDone",
+    type: "string",
+    title: "Append on final workflow item?",
+    description: "When the task is or workflow reaches the final state, this text will be appended. You can use the following variables: {DATE}, {TIME} and {CurrentPage}. Leave blank to disable",
+    default: ""
+
   },
   {
     key: "CommaSeparatedOptions2",
@@ -79,6 +146,13 @@ let settings: SettingSchemaDesc[] = [
     type: "string",
     title: "keyboard shortcut for Workflow 2",
     description: "Choose your desired keyboard shortcut to toggle the changes",
+    default: ""
+  },
+  {
+    key: "AppendOnDone2",
+    type: "string",
+    title: "Append on final workflow item?",
+    description: "When the task is or workflow reaches the final state, this text will be appended. You can use the following variables: {DATE}, {TIME} and {CurrentPage}. Leave blank to disable",
     default: ""
   },
   {
@@ -105,6 +179,13 @@ let settings: SettingSchemaDesc[] = [
     default: ""
   },
   {
+    key: "AppendOnDone3",
+    type: "string",
+    title: "Append on final workflow item?",
+    description: "When the task is or workflow reaches the final state, this text will be appended. You can use the following variables: {DATE}, {TIME} and {CurrentPage}. Leave blank to disable",
+    default: ""
+  },
+  {
     key: "CommaSeparatedOptions4",
     type: "string",
     title: "Options for Workflow 4",
@@ -125,6 +206,13 @@ let settings: SettingSchemaDesc[] = [
     type: "string",
     title: "keyboard shortcut for Workflow 4",
     description: "Choose your desired keyboard shortcut to toggle the changes",
+    default: ""
+  },
+  {
+    key: "AppendOnDone4",
+    type: "string",
+    title: "Append on final workflow item?",
+    description: "When the task is or workflow reaches the final state, this text will be appended. You can use the following variables: {DATE}, {TIME} and {CurrentPage}. Leave blank to disable",
     default: ""
   },
   {
@@ -150,6 +238,13 @@ let settings: SettingSchemaDesc[] = [
     description: "Choose your desired keyboard shortcut to toggle the changes",
     default: ""
   },
+  {
+    key: "AppendOnDone5",
+    type: "string",
+    title: "Append on final workflow item?",
+    description: "When the task is or workflow reaches the final state, this text will be appended. You can use the following variables: {DATE}, {TIME} and {CurrentPage}. Leave blank to disable",
+    default: ""
+  },
 ]
 logseq.useSettingsSchema(settings)
 const main = async () => {
@@ -167,17 +262,17 @@ const main = async () => {
       }
     }, async (e) => {
       const blocks = await logseq.Editor.getSelectedBlocks()
-      if (blocks.length > 0 ){
+      if (blocks != undefined && blocks.length > 0){
         for (const x in blocks){
-          toggleState(logseqSettings["CommaSeparatedOptions"], blocks[x].uuid)
+          toggleState(logseqSettings["CommaSeparatedOptions"], blocks[x].uuid, logseqSettings["AppendOnDone"])
         }
       }
       else if (logseq.settings["ToggleStateofBlockBelow"] == "Only Change current block") {
-        toggleState(logseqSettings["CommaSeparatedOptions"], e.uuid)
+        toggleState(logseqSettings["CommaSeparatedOptions"], e.uuid, logseqSettings["AppendOnDone"])
       }
       else {
-        toggleState(logseqSettings["CommaSeparatedOptions"], e.uuid)
-        toggleState(logseqSettings["CommaSeparatedOptions"], (await logseq.Editor.getNextSiblingBlock(e.uuid)).uuid)
+        toggleState(logseqSettings["CommaSeparatedOptions"], e.uuid, logseqSettings["AppendOnDone"])
+        toggleState(logseqSettings["CommaSeparatedOptions"], (await logseq.Editor.getNextSiblingBlock(e.uuid)).uuid, logseqSettings["AppendOnDone"])
       }
     })
   };
@@ -190,11 +285,11 @@ const main = async () => {
       }
     }, async (e) => {
       if (logseq.settings["ToggleStateofBlockBelow2"] == "Only Change current block") {
-        toggleState(logseqSettings["CommaSeparatedOptions2"], e.uuid)
+        toggleState(logseqSettings["CommaSeparatedOptions2"], e.uuid, logseqSettings["AppendOnDone2"])
       }
       else {
-        toggleState(logseqSettings["CommaSeparatedOptions2"], e.uuid)
-        toggleState(logseqSettings["CommaSeparatedOptions2"], (await logseq.Editor.getNextSiblingBlock(e.uuid)).uuid)
+        toggleState(logseqSettings["CommaSeparatedOptions2"], e.uuid, logseqSettings["AppendOnDone2"])
+        toggleState(logseqSettings["CommaSeparatedOptions2"], (await logseq.Editor.getNextSiblingBlock(e.uuid)).uuid, logseqSettings["AppendOnDone2"])
       }
     });
   }
@@ -207,11 +302,11 @@ const main = async () => {
       }
     }, async (e) => {
       if (logseq.settings["ToggleStateofBlockBelow3"] == "Only Change current block") {
-        toggleState(logseqSettings["CommaSeparatedOptions3"], e.uuid)
+        toggleState(logseqSettings["CommaSeparatedOptions3"], e.uuid, logseqSettings["AppendOnDone3"])
       }
       else {
-        toggleState(logseqSettings["CommaSeparatedOptions3"], e.uuid)
-        toggleState(logseqSettings["CommaSeparatedOptions3"], (await logseq.Editor.getNextSiblingBlock(e.uuid)).uuid)
+        toggleState(logseqSettings["CommaSeparatedOptions3"], e.uuid, logseqSettings["AppendOnDone3"])
+        toggleState(logseqSettings["CommaSeparatedOptions3"], (await logseq.Editor.getNextSiblingBlock(e.uuid)).uuid, logseqSettings["AppendOnDone3"])
       }
     });
   }
@@ -224,11 +319,11 @@ const main = async () => {
       }
     }, async (e) => {
       if (logseq.settings["ToggleStateofBlockBelow4"] == "Only Change current block") {
-        toggleState(logseqSettings["CommaSeparatedOptions4"], e.uuid)
+        toggleState(logseqSettings["CommaSeparatedOptions4"], e.uuid, logseqSettings["AppendOnDone4"])
       }
       else {
-        toggleState(logseqSettings["CommaSeparatedOptions4"], e.uuid)
-        toggleState(logseqSettings["CommaSeparatedOptions4"], (await logseq.Editor.getNextSiblingBlock(e.uuid)).uuid)
+        toggleState(logseqSettings["CommaSeparatedOptions4"], e.uuid, logseqSettings["AppendOnDone4"])
+        toggleState(logseqSettings["CommaSeparatedOptions4"], (await logseq.Editor.getNextSiblingBlock(e.uuid)).uuid, logseqSettings["AppendOnDone4"])
       }
     });
   }
@@ -241,11 +336,11 @@ const main = async () => {
       }
     }, async (e) => {
       if (logseq.settings["ToggleStateofBlockBelow5"] == "Only Change current block") {
-        toggleState(logseqSettings["CommaSeparatedOptions5"], e.uuid)
+        toggleState(logseqSettings["CommaSeparatedOptions5"], e.uuid, logseqSettings["AppendOnDone5"])
       }
       else {
-        toggleState(logseqSettings["CommaSeparatedOptions5"], e.uuid)
-        toggleState(logseqSettings["CommaSeparatedOptions5"], (await logseq.Editor.getNextSiblingBlock(e.uuid)).uuid)
+        toggleState(logseqSettings["CommaSeparatedOptions5"], e.uuid, logseqSettings["AppendOnDone5"])
+        toggleState(logseqSettings["CommaSeparatedOptions5"], (await logseq.Editor.getNextSiblingBlock(e.uuid)).uuid, logseqSettings["AppendOnDone5"])
       }
     });
   }
